936d895cbad5527214293448ee0e8e8bb96dac15
Wayne <22000210@myrp.edu.sg>
2023-05-18 12:36:16 +0800

    Initial Commit, Completed

---
+++ app/src/main/java/sg/edu/rp/c346/id22000210/demodatapassingtest/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.id22000210.demodatapassingtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Event handling Step 1
+    Button btnPassInteger;
+    Button btnPassChar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Event handling Step 2
+        btnPassInteger = findViewById(R.id.buttonPassInteger);
+        btnPassChar = findViewById(R.id.buttonPassChar);
+
+        //Event handling Step 3
+        btnPassInteger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //this is to link MainActivity to Second Activity by using Intent
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("value", 1);
+                startActivity(intent);
+            }
+        });
+
+        btnPassChar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //this is to link MainActivity to Third Activity by using Intent
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                intent.putExtra("value", 'a');
+                startActivity(intent);
+            }
+        });
+
+    }
+}
